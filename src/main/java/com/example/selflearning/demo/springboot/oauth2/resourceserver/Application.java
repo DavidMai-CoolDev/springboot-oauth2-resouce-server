6b1ee35a575e15b2931d9101e14643846ff50d56
MaiDuy <maiduy191992@gmail.com>
2020-07-01 07:48:41 +0700

    initial commit

---
+++ src/main/java/com/example/selflearning/demo/springboot/oauth2/resourceserver/Application.java
@@ -0,0 +1,12 @@
+package com.example.selflearning.demo.springboot.oauth2.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
